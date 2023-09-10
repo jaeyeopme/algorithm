29c9d70e24171276b81dbf1e0b1d74bfde67dcce
jaeyeopme <jaeyeopme@gmail.com>
2023-09-10 16:30:34 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 104 ms, Memory: 12756 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -1,21 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        StringBuilder re = new StringBuilder();
         
         for (char c : str.toCharArray()) {
-            if (Character.isUpperCase(c)) {
-                re.append(Character.toLowerCase(c));
+            if ('A' <= c && c <= 'Z') {
+                int i = c - 'A';
+                System.out.print((char) ('a' + i));
             } else {
-                re.append(Character.toUpperCase(c));
+                int i = c - 'a';
+                System.out.print((char) ('A' + i));
             }
         }
-        
-        System.out.print(re);
-    }    
-    
+    }
 }
