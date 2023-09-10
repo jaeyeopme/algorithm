465300eb92ca0117a9c958b6fa611a499c80f752
jaeyeopme <jaeyeopme@gmail.com>
2023-09-10 20:39:08 +0900

    [Bronze I] Title: 단어 공부, Time: 336 ms, Memory: 30484 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -6,27 +6,27 @@ public class Main {
         char[] a = sc.next().toUpperCase().toCharArray();
 
         if (a.length == 1) {
-            System.out.println(a[0]);
+            System.out.print(a[0]);
             return;
         }
 
         // A - Z
         int[] b = new int[26];
-        for (char e : a) b[e - 'A']++;
+        for (char c : a) b[c - 'A']++;
 
-        int maxCount = 0;
+        int d = 0;
         char ans = 'A';
 
         for (int i = 0; i < b.length; i++) {
-            if (b[i] > maxCount) {
-                maxCount = b[i];
+            if (b[i] > d) {
+                d = b[i];
                 ans = (char) ('A' + i);
-            } else if (b[i] == maxCount) {
-                maxCount = b[i];
+            } else if (b[i] == d) {
+                d = b[i];
                 ans = '?';
             }
         }
 
-        System.out.println(ans);
+        System.out.print(ans);
     }
 }
