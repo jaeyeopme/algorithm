ba3584df3e4791ce6a4c905c7421313de1eb21ce
Jaeyeop Lee <67671991+jaeyeopme@users.noreply.github.com>
2023-09-10 20:36:03 +0900

    [Bronze I] Title: 단어 공부, Time: 344 ms, Memory: 30764 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -10,34 +10,21 @@ public class Main {
             return;
         }
 
-        // first, second
-        int c = 0, d = 1;
-
         // A - Z
         int[] b = new int[26];
         for (char e : a) b[e - 'A']++;
 
-        char ans;
+        int maxCount = 0;
+        char ans = 'A';
 
-        if (b[1] > b[0]) {
-            c = 1;
-            d = 0;
-        }
-
-        for (int i = 2; i < b.length; i++) {
-            if (b[i] > b[c]) {
-                c = i;
-            } else if (b[i] > b[d]) {
-                d = i;
+        for (int i = 0; i < b.length; i++) {
+            if (b[i] > maxCount) {
+                maxCount = b[i];
+                ans = (char) ('A' + i);
+            } else if (b[i] == maxCount) {
+                maxCount = b[i];
+                ans = '?';
             }
-        }
-
-        if (b[c] == b[d]) {
-            ans = '?';
-        } else if (b[c] > b[d]) {
-            ans = (char) ('A' + c);
-        } else {
-            ans = (char) ('A' + d);
         }
 
         System.out.println(ans);
