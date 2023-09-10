4e479f42d40c0a3daceb4e7c293fa47d6e3052f7
Jaeyeop Lee <67671991+jaeyeopme@users.noreply.github.com>
2023-09-10 22:00:00 +0900

    [Silver V] Title: 문서 검색, Time: 112 ms, Memory: 12856 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -5,18 +5,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String a = sc.nextLine();
         String b = sc.nextLine();
-        int c = 0;
-        int ans = 0;
-
-        while (true) {
-            int d = a.indexOf(b, c);
-
-            if (d == -1) break;
-
-            c = d + b.length();
-            ans++;
-        }
-
-        System.out.print(ans);
+        int c = a.length() - a.replace(b, "").length();
+        
+        System.out.print(c / b.length());
     }
 }
