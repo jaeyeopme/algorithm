78b846b217728ac02b5bfa0c322c57d222981cfb
jaeyeopme <jaeyeopme@gmail.com>
2023-09-10 21:39:40 +0900

    [Silver V] Title: 문서 검색, Time: 116 ms, Memory: 13108 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -6,17 +6,16 @@ public class Main {
         String a = sc.nextLine();
         String b = sc.nextLine();
         int c = 0;
+        int d = a.length() - b.length() + 1;
         int ans = 0;
 
-        for (int i = 0; i < a.length() - b.length() + 1; i++) {
+        for (int i = 0; i < d; i++) {
             if (c != 0) {
                 c--;
                 continue;
             }
 
-            String d = a.substring(i, i + b.length());
-
-            if (b.equals(d))  {
+            if (b.equals(a.substring(i, i + b.length())))  {
                 ans++;
                 c = b.length() - 1;
             }
