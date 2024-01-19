d850ac17405fee0ff9f89ecb0ae6fee3beaf2040
Jaeyeop Lee <jaeyeopme@gmail.com>
2024-01-20 03:14:37 +0900

    [Bronze I] Title: 성 지키기, Time: 120 ms, Memory: 12968 KB -BaekjoonHub

--- 백준/Bronze/1236. 성 지키기/성 지키기.java
+++ 백준/Bronze/1236. 성 지키기/성 지키기.java
@@ -11,31 +11,25 @@ public class Main {
         boolean[] width = new boolean[M];
         boolean[] height = new boolean[N];
 
+        int wCount = 0;
+        int hCount = 0;
+
         for (int i = 0; i < N; i++) {
             char[] line = scan.next().toCharArray();
             for (int j = 0; j < M; j++) {
                 if (line[j] == 'X') {
-                    height[i] = true;
-                    width[j] = true;
+                    if (!height[i]) {
+                        height[i] = true;
+                        hCount++;
+                    }
+                    if (!width[j]) {
+                        width[j] = true;
+                        wCount++;
+                    }
                 }
             }
         }
 
-        int wCount = 0;
-        int hCount = 0;
-
-        for (int i = 0; i < M; i++) {
-            if (!width[i]) {
-                wCount++;
-            }
-        }
-
-        for (int i = 0; i < N; i++) {
-            if (!height[i]) {
-                hCount++;
-            }
-        }
-
-        System.out.print(Math.max(wCount, hCount));
+        System.out.print(Math.max(M - wCount, N - hCount));
     }
 }
