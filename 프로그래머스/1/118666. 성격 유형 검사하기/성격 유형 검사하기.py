99c16fad4abb1eae4dfcb00f8b53f902e0cb2ce3
Jaeyeop Lee <jaeyeopme@gmail.com>
2024-03-27 19:00:52 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.39 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -0,0 +1,43 @@
+def solution(survey, choices):
+    s_dict = {
+        "R" : 0, 
+        "T" : 0, 
+        "C" : 0, 
+        "F" : 0, 
+        "J" : 0, 
+        "M" : 0, 
+        "A" : 0,
+        "N" : 0
+    }
+    
+    for i, s in enumerate(survey):
+        if choices[i] == 4:
+            continue
+        elif choices[i] < 4:
+            s_dict[s[0]] = s_dict[s[0]] + ((choices[i] * -1) + 4)
+        else:
+            s_dict[s[1]] = s_dict[s[1]] + (choices[i] - 4)
+    
+    answer = ""
+    
+    if s_dict["R"] >= s_dict["T"]:
+        answer += "R"
+    else:
+        answer += "T"
+        
+    if s_dict["C"] >= s_dict["F"]:
+        answer += "C"
+    else:
+        answer += "F"
+        
+    if s_dict["J"] >= s_dict["M"]:
+        answer += "J"
+    else:
+        answer += "M"
+        
+    if s_dict["A"] >= s_dict["N"]:
+        answer += "A"
+    else:
+        answer += "N"
+        
+    return answer
