23df4e7ebdba92b669afad92d9de1a74e3f0bcf7
Jaeyeop Lee <jaeyeopme@gmail.com>
2024-04-01 20:10:19 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.54 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -0,0 +1,10 @@
+def solution(numbers):
+    # 모든 조합을 만들어(2중 순회 O(N^2)) 오름차순 정렬(O(NlogN))
+    # 최대 길이 100이하 100^2 = 10000
+    answer = []
+    
+    for i in range(len(numbers) - 1):
+        for j in range(i + 1, len(numbers)):
+            answer.append(numbers[i] + numbers[j])
+            
+    return sorted(set(answer))
