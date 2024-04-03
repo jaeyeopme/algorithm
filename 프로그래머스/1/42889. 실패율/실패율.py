1fd729cd49b0c986a48134b2647405b2a0662f8f
jaeyeopme <jaeyeopme@gmail.com>
2024-04-03 14:38:41 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -0,0 +1,16 @@
+def solution(N, stages):
+    answer = {}
+
+    for i in range(1, N + 1):
+        a, b = 0, 0
+
+        for j in stages:
+            if i == j:
+                a += 1
+            elif i < j:
+                b += 1
+        
+        b += a
+        answer[i] = b if b == 0 else a / b
+
+    return [key for key, _ in sorted(answer.items(), key=lambda x: x[1], reverse=True)]
