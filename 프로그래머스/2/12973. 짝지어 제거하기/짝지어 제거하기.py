65b2b2c9f5c1049918e8de62f03b54338f7b9901
jaeyeopme <jaeyeopme@gmail.com>
2024-04-10 16:01:52 +0900

    [level 2] Title: 짝지어 제거하기, Time: 87.94 ms, Memory: 14.7 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -3,12 +3,13 @@ def solution(s):
     stack = []
     
     for c in s:
-        # 비어있지 않을 때
+        # 비어있지 않은 경우
         if stack and stack[-1] == c:
             stack.pop()
         else:
             stack.append(c)
-            
+
+    # 비어있는 경우
     if not stack:
         answer = 1
         
