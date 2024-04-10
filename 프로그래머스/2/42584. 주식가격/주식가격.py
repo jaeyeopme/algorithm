7a0c8cb4b5f35f0eb92a5528bb463a5269474607
jaeyeopme <jaeyeopme@gmail.com>
2024-04-10 18:20:34 +0900

    [level 2] Title: 주식가격, Time: 16.15 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,15 +1,21 @@
 def solution(prices):
-    answer = []
+    n = len(prices)
+    answer = [0] * n
+    i_stack = [0]
     
-    for i in range(len(prices)):
-        count = 0
-        
-        for j in range(i + 1, len(prices)):
-            count += 1
-            
-            if prices[i] > prices[j]:
-                break
-        
-        answer.append(count)
+    for i in range(1, n):
+        # 가격이 떨어졌다면 이전 인덱스의 값이 얼마나 가격을 유지했는지 계산
+        while i_stack and prices[i] < prices[i_stack[-1]]:
+            j = i_stack.pop()
+            # 이전 인덱스의 유지 기간 = 현재 인덱스 - 이전 인덱스
+            answer[j] = i - j
+        # 떨어지지 않았다면 인덱스 추가
+        i_stack.append(i)
+    
+    # 가격이 떨어지지 않은 인덱스의 값이 얼마나 가격을 유지했는지 계산
+    while i_stack:
+        j = i_stack.pop()
+        # 전체 기간 - 1 - 인덱스
+        answer[j] = n - 1 - j
             
     return answer
