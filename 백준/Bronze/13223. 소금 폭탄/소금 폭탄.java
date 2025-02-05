4f9f53fe76519fdabddb881f4a146a06b12563e7
jaeyeopme <jaeyeopme@gmail.com>
2025-02-05 21:36:44 +0900

    [Bronze III] Title: 소금 폭탄, Time: 88 ms, Memory: 12904 KB -BaekjoonHub

--- 백준/Bronze/13223. 소금 폭탄/소금 폭탄.java
+++ 백준/Bronze/13223. 소금 폭탄/소금 폭탄.java
@@ -1,25 +1,50 @@
 import java.util.Scanner;
 
 public class Main {
+    
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String[] a = sc.nextLine().split(":");
-        String[] b = sc.nextLine().split(":");
-        
-        int s = Integer.parseInt(b[2]) - Integer.parseInt(a[2]);
-        int m = Integer.parseInt(b[1]) - Integer.parseInt(a[1]);
-        int h = Integer.parseInt(b[0]) - Integer.parseInt(a[0]);
-        
-        if (s < 0) {
-            m--;
-            s += 60;
+        Scanner scan = new Scanner(System.in);
+        String[] sTime1 = scan.nextLine().split(":");
+        String[] sTime2 = scan.nextLine().split(":");
+        int[] ans = new int[3];
+        ans[0] = 24;
+
+        if (!(sTime1[2].equals(sTime2[2]) &&
+                sTime1[1].equals(sTime2[1]) &&
+                sTime1[0].equals(sTime2[0]))) {
+            int[] time1 = new int[3];
+            int[] time2 = new int[3];
+
+            for (int i = 0; i < 3; i++) {
+                time1[i] = Integer.parseInt(sTime1[i]);
+                time2[i] = Integer.parseInt(sTime2[i]);
+            }
+
+            // second
+            if (time2[2] < time1[2]) {
+                time2[1]--;
+                ans[2] = (time2[2] + 60) - time1[2];
+            } else {
+                ans[2] = time2[2] - time1[2];
+            }
+
+            // minute
+            if (time2[1] < time1[1]) {
+                time2[0]--;
+                ans[1] = (time2[1] + 60) - time1[1];
+            } else {
+                ans[1] = time2[1] - time1[1];
+            }
+
+            // hour
+            if (time2[0] < time1[0]) {
+                ans[0] = (time2[0] + 24) - time1[0];
+            } else {
+                ans[0] = time2[0] - time1[0];
+            }
         }
-        if (m < 0) {
-            h--;
-            m += 60;
-        }
-        if (h < 0 || h + m + s == 0) h += 24;
-        
-        System.out.printf("%02d:%02d:%02d", h, m, s);
+
+        System.out.printf("%02d:%02d:%02d", ans[0], ans[1], ans[2]);
     }
+    
 }
