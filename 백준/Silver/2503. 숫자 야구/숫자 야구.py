e780f9923ce705bfb7ab30bbdb46e2e64fe2b952
jaeyeopme <jaeyeopme@gmail.com>
2025-02-16 14:09:11 +0900

    [Silver III] Title: 숫자 야구, Time: 92 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -1,45 +1,36 @@
 N = int(input())
-hints = [list(map(int, input().split())) for _ in range(N)]
-answer = 0
+guesses = [list(map(int, input().split())) for _ in range(N)]
+answer  = 0
 
 for a in range(1, 10):
     for b in range(1, 10):
         for c in range(1, 10):
-            # 서로 다른 숫자 세개
             if a == b or a == c or b == c:
                 continue
+            count = 0
 
-            cnt = 0
-
-            for hint in hints:
-                numbers = list(str(hint[0]))
-                a1, b1, c1 = map(int, numbers)
-
-                strike = hint[1]
-                ball = hint[2]
-
-                strike_count = 0
-                ball_count = 0
-
-                if a == a1:
-                    strike_count += 1
-                if b == b1:
-                    strike_count += 1
-                if c == c1:
-                    strike_count += 1
-
-                if a != a1 and str(a) in numbers:
-                    ball_count += 1    
-                if b != b1 and str(b) in numbers:
-                    ball_count += 1    
-                if c != c1 and str(c) in numbers:
-                    ball_count += 1    
+            for guess in guesses:
+                number, strike, ball = guess
+                a1, b1, c1 = map(int, str(number))
                 
-                if strike == strike_count and ball == ball_count:
-                    cnt += 1
+                if a == a1:
+                    strike -= 1
+                if b == b1:
+                    strike -= 1
+                if c == c1:
+                    strike -= 1
 
-            # 모든 힌트를 통과했다면
-            if cnt == N:
+                if a == b1 or a == c1:
+                    ball -= 1
+                if b == a1 or b == c1:
+                    ball -= 1
+                if c == a1 or c == b1:
+                    ball -= 1
+
+                if strike == 0 and ball == 0:
+                    count += 1
+
+            if count == N:
                 answer += 1
 
 print(answer)
