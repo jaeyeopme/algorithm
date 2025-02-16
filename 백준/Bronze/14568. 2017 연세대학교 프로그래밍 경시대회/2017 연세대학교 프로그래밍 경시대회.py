9fefea020c6316bd5522688680fd67fa070f8336
jaeyeopme <jaeyeopme@gmail.com>
2025-02-16 12:47:13 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 132 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
@@ -1,10 +1,13 @@
 N = int(input())
-result = 0
+ans = 0
 
+# 택희
 for A in range(1, N):
+    # 영훈
     for B in range(1, N):
+        # 남규
         for C in range(1, N):
-            if A + B + C == N and A % 2 == 0 and C >= B + 2:
-                result += 1
+            if A + B + C == N and C >= B + 2 and A % 2 == 0:
+                ans += 1
 
-print(result)
+print(ans)
