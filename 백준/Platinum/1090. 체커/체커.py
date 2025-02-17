663f0991811c518939da65fd66aea14e177576d2
jaeyeopme <jaeyeopme@gmail.com>
2025-02-17 14:26:42 +0900

    [Platinum IV] Title: 체커, Time: 96 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Platinum/1090. 체커/체커.py
+++ 백준/Platinum/1090. 체커/체커.py
@@ -5,10 +5,10 @@ ans = [float('inf')] * N
 
 for cx, cy in candidates:
     dis = sorted(abs(cx - x) + abs(cy - y) for x, y in checks)
-    total = 0
+    acc = 0
 
     for i in range(N):
-        total += dis[i]
-        ans[i] = min(ans[i], total)
+        acc += dis[i]
+        ans[i] = min(ans[i], acc)
 
 print(*ans)
