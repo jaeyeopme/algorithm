e5782e7602e9b56910521dd33ed070490cba6c2a
jaeyeopme <jaeyeopme@gmail.com>
2025-02-20 15:06:59 +0900

    [Silver II] Title: 연속합, Time: 72 ms, Memory: 39096 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,7 +1,9 @@
+# 연속된 수 중 가장 큰 합을 구해야 함
 N = int(input())
 arr = list(map(int, input().split()))
 prefix = [-1001] * (N + 1)
 
+# 누적할 값이 누적된 값보다 크다면 누적된 값을 버림
 for i in range(N):
     prefix[i + 1] = max(prefix[i] + arr[i], arr[i])
 
