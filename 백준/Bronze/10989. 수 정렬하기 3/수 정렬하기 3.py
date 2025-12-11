4cc113885003863a4e452d04ab17907a46c8dd53
jaeyeopme <jaeyeopme@gmail.com>
2025-12-11 15:34:31 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 7864 ms, Memory: 34900 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -4,11 +4,11 @@ from collections import defaultdict
 input = sys.stdin.readline
 
 N = int(input())
-map = defaultdict(int)
+count = [0] * 10001
 
 for _ in range(N):
-    map[int(input())] += 1
+    count[int(input())] += 1
 
-for k in sorted(list(map.keys())):
-    for i in range(map[k]):
-        print(k)
+for i in range(10001):
+    for _ in range(count[i]):
+        print(i)
