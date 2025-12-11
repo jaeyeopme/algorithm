b8f3c061e1d3c6d41cea6f8d108fefc08c6480fe
jaeyeopme <jaeyeopme@gmail.com>
2025-12-11 19:40:52 +0900

    [Silver III] Title: 두 수의 합, Time: 72 ms, Memory: 47024 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -2,22 +2,14 @@ import sys
 
 input = sys.stdin.readline
 
-n = int(input())
-na = [False] * 1000001
-
-a = sorted(list(map(int, input().split())))
+int(input())
+a = set(map(int, input().split()))
 x = int(input())
+
 count = 0
 
-# 1000000
 for i in a:
-    na[i] = True
-
-for i in a:
-    j = x - i
-
-    if i < j <= 1000001 and na[j]:
+    if x - i in a:
         count += 1
-        na[j] = False
 
-print(count)
+print(count // 2)
