8ddbcf1cc772711d2722917043840982469eec0c
jaeyeopme <jaeyeopme@gmail.com>
2025-12-15 20:02:45 +0900

    [Bronze I] Title: 유레카 이론, Time: 88 ms, Memory: 36176 KB -BaekjoonHub

--- 백준/Bronze/10448. 유레카 이론/유레카 이론.py
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.py
@@ -2,13 +2,13 @@ import sys
 
 input = sys.stdin.readline
 arr = [int(input()) for _ in range(int(input()))]
-
+all_permu = [int((n * (n + 1)) / 2) for n in range(1, 45)]
+len_permu = len(all_permu)
 permu = []
 
-for a in range(1, 45):
-    for b in range(1, 45):
-        for c in range(1, 45):
-            permu.append(sum([int((n * (n + 1)) / 2) for n in [a, b, c]]))
+for a in range(len_permu):
+    for b in range(len_permu):
+        for c in range(len_permu):
+            permu.append(all_permu[a] + all_permu[b] + all_permu[c])
 
-for x in arr:
-    print(int(x in permu))
+print(*[int(x in permu) for x in arr], sep="\n")
