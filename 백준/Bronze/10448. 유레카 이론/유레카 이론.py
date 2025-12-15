f151cff025d52a36af8f9cea754c22f3a2cd4af4
jaeyeopme <jaeyeopme@gmail.com>
2025-12-15 20:59:50 +0900

    [Bronze I] Title: 유레카 이론, Time: 72 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/10448. 유레카 이론/유레카 이론.py
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.py
@@ -7,15 +7,13 @@ permu = []
 
 for a in range(len_permu):
     for b in range(len_permu):
-        if all_permu[a] + all_permu[b] > 1000:
+        ab = all_permu[a] + all_permu[b]
+        if ab  > 1000:
             continue
-            
-        for c in range(len_permu):
-            sum = all_permu[a] + all_permu[b] + all_permu[c]
-            if sum > 1000:
-                continue
 
-            if sum <= 1000: permu.append(sum)
+        for c in range(len_permu):
+            abc = ab + all_permu[c]
+            if abc <= 1000: permu.append(abc)
 
 
 print(*[int(x in permu) for x in [int(input()) for _ in range(int(input()))]], sep="\n")
