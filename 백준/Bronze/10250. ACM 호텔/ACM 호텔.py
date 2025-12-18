62e562b6f7198323a085a2baef4d10c22537dd13
jaeyeopme <jaeyeopme@gmail.com>
2025-12-18 17:49:05 +0900

    [Bronze III] Title: ACM 호텔, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -6,13 +6,7 @@ T = int(input())
 
 for _ in range(T):
     H, W, N = map(int, input().split())
-    Y, X = 0, 1
-    for i in range(N):
-        Y += 1
-        if Y == H:
-            if i == N - 1:
-                break
-            Y = 0
-            X += 1
-
-    print(f"{Y}{X:02d}")
+    if N % H == 0:
+        print(f"{H}{N // H:02d}")
+        continue
+    print(f"{N % H}{N // H + 1:02d}")
