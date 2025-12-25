c4441d6f4e5d30404265136d3182e2b3c420ee3d
jaeyeopme <jaeyeopme@gmail.com>
2025-12-25 20:37:35 +0900

    [Gold V] Title: 회의실 배정, Time: 248 ms, Memory: 61144 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -3,11 +3,11 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-meetings = sorted([list(map(int, input().split())) for _ in range(N)], key=lambda x: (x[1], x[0]))
+meetings = [list(map(int, input().split())) for _ in range(N)]
 
 last_end_time, count = 0, 0
 
-for start, end in meetings:
+for start, end in sorted(meetings, key=lambda x: (x[1], x[0])):
     if start >= last_end_time:
         count += 1
         last_end_time = end
