9db42c426d28ec469f4f1bd4331da4388df5aa1e
jaeyeopme <jaeyeopme@gmail.com>
2026-01-07 20:58:46 +0900

    [Silver IV] Title: 수 찾기, Time: 148 ms, Memory: 52416 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -3,22 +3,8 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-A = sorted(list(map(int, input().split())))
+A = set(map(int, input().split()))
 M = int(input())
 T = list(map(int, input().split()))
 
-for t in T:
-    found = False
-    l, r = 0, N - 1
-
-    while l <= r:
-        mid = (l + r) // 2
-        if t < A[mid]:
-            r = mid - 1
-        elif t > A[mid]:
-            l = mid + 1
-        else:
-            found = True
-            break
-
-    print(1 if found else 0)
+[print(1 if t in A else 0) for t in T]
