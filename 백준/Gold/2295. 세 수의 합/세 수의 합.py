8d56c4aa9071632d7d1a074058b16d9ad4750ca0
jaeyeopme <jaeyeopme@gmail.com>
2026-01-08 09:20:54 +0900

    [Gold IV] Title: 세 수의 합, Time: 220 ms, Memory: 77448 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.py
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.py
@@ -4,12 +4,7 @@ input = sys.stdin.readline
 
 N = int(input())
 U = sorted([int(input()) for _ in range(N)], reverse=True)
-XY = set()
-
-# x + y = k - z
-for i in range(N):
-    for j in range(i, N):
-        XY.add(U[i] + U[j])
+XY = set([U[i] + U[j] for i in range(N) for j in range(i, N)])
 
 for k in U:
     found = False
