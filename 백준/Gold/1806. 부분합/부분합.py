61e042d794573f197265a1fe2eae6687d2f8e0ca
jaeyeopme <jaeyeopme@gmail.com>
2026-01-23 16:32:12 +0900

    [Gold IV] Title: 부분합, Time: 96 ms, Memory: 42168 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -5,18 +5,15 @@ input = sys.stdin.readline
 N, S = map(int, input().split())
 A = list(map(int, input().split()))
 
-start, end = 0, 0
-current_sum, min_len = 0, float("inf")
+start = 0
+current_sum, min_len = 0, N + 1
 
-while True:
-  if current_sum >= S:
-    min_len = min(min_len, end - start)
+for end in range(N):
+  current_sum += A[end]
+
+  while current_sum >= S:
+    min_len = min(min_len, end - start + 1)
     current_sum -= A[start]
     start += 1
-  elif end < N:
-    current_sum += A[end]
-    end += 1
-  else:
-    break
 
-print(0 if min_len == float("inf") else min_len)
+print(0 if min_len == N + 1 else min_len)
