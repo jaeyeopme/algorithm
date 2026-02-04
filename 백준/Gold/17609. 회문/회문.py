c91926124778f6564414faf42243bfe42bba3a38
jaeyeopme <jaeyeopme@gmail.com>
2026-02-04 15:47:57 +0900

    [Gold V] Title: 회문, Time: 144 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.py
+++ 백준/Gold/17609. 회문/회문.py
@@ -2,12 +2,18 @@ import sys
 
 input = sys.stdin.readline
 
-T = int(input())
-S = [input().strip() for _ in range(T)]
-ans = []
 
-for s in S:
-    left_diff = 0
+def is_palindrome(s, left, right):
+    while left < right:
+        if s[left] == s[right]:
+            left += 1
+            right -= 1
+        else:
+            return False
+    return True
+
+
+def solve(s):
     left, right = 0, len(s) - 1
 
     while left < right:
@@ -16,36 +22,17 @@ for s in S:
             right -= 1
             continue
 
-        if s[left + 1] == s[right]:
-            left += 1
-            left_diff += 1
-        elif s[left] == s[right - 1]:
-            right -= 1
-            left_diff += 1
+        check_left = is_palindrome(s, left + 1, right)
+        check_right = is_palindrome(s, left, right - 1)
+
+        if check_left or check_right:
+            return 1
         else:
-            left_diff = 2
-            break
+            return 2
 
-    right_diff = 0
-    left, right = 0, len(s) - 1
+    return 0
 
-    while left < right:
-        if s[left] == s[right]:
-            left += 1
-            right -= 1
-            continue
 
-        if s[left] == s[right - 1]:
-            right -= 1
-            right_diff += 1
-        elif s[left + 1] == s[right]:
-            left += 1
-            right_diff += 1
-        else:
-            right_diff = 2
-            break
-
-    v = min(left_diff, right_diff)
-    ans.append(v if v < 2 else 2)
-
-print(*ans)
+for _ in range(int(input())):
+    s = input().strip()
+    print(solve(s))
